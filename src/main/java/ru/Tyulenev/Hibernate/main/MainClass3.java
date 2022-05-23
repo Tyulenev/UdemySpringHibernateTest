cbde75572c7c9dbf4f242b93c22e3a41b30d414b
Alexey Tyulenev <alexey.tyulenev@q-systems.ru>
2022-05-23 17:37:27 +0300

    Редактирвоание/удаление данных из таблицу и аннотация

--- src/main/java/ru/Tyulenev/Hibernate/main/MainClass3.java
+++ src/main/java/ru/Tyulenev/Hibernate/main/MainClass3.java
@@ -7,8 +7,8 @@ import ru.Tyulenev.Hibernate.Entity.EmployeesEntity;
 
 import java.util.List;
 
-//Получение данных из БД
-public class MainClass2 {
+//Обновление данных
+public class MainClass3 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration().
                 configure("hibernateMSSQL.cfg.xml")
@@ -19,15 +19,13 @@ public class MainClass2 {
         try {
             Session session_MS_SQL = factory.getCurrentSession();
             session_MS_SQL.beginTransaction();
-            List<EmployeesEntity> emps = session_MS_SQL.createQuery("from EmployeesEntity")
-                    .getResultList();   //HQL запрос
-            for (EmployeesEntity emp:emps) {
-                System.out.println(emp);
-            }
-            System.out.println("Всего работников в таблице employees_test: " + emps.size());
 
+//            EmployeesEntity emp = session_MS_SQL.get(EmployeesEntity.class, 1);
+//            emp.setSalary(emp.getSalary()+20);
+
+            session_MS_SQL.createQuery("update EmployeesEntity set salary=2223" +
+                    "where name = 'Ivan'").executeUpdate();
             session_MS_SQL.getTransaction().commit();
-
             System.out.println("Done");
         }
         finally {
--- src/main/java/ru/Tyulenev/Hibernate/main/MainClass4.java
+++ src/main/java/ru/Tyulenev/Hibernate/main/MainClass4.java
@@ -5,10 +5,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.Tyulenev.Hibernate.Entity.EmployeesEntity;
 
-import java.util.List;
-
-//Получение данных из БД
-public class MainClass2 {
+//Удаление объектов из базы
+public class MainClass4 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration().
                 configure("hibernateMSSQL.cfg.xml")
@@ -19,12 +17,12 @@ public class MainClass2 {
         try {
             Session session_MS_SQL = factory.getCurrentSession();
             session_MS_SQL.beginTransaction();
-            List<EmployeesEntity> emps = session_MS_SQL.createQuery("from EmployeesEntity")
-                    .getResultList();   //HQL запрос
-            for (EmployeesEntity emp:emps) {
-                System.out.println(emp);
-            }
-            System.out.println("Всего работников в таблице employees_test: " + emps.size());
+
+//            EmployeesEntity emp = session_MS_SQL.get(EmployeesEntity.class, 5);
+//            session_MS_SQL.delete(emp);
+
+            session_MS_SQL.createQuery("delete EmployeesEntity " +
+                    "where name = 'Helen'").executeUpdate();
 
             session_MS_SQL.getTransaction().commit();
 
